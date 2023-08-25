8844e8544fef4989d4fa5228c2d814e6e8b019d7
bowei.li <bowei.li@i-tudou.com>
2023-08-25 18:48:55 +0800

    'refactor-rm-unuse-file'

--- src/main/java/com/lonicera/factory/Recycleable.java
+++
@@ -1,5 +0,0 @@
-package com.lonicera.factory;
-
-public interface Recycleable {
-  void recycle();
-}
--- src/main/java/com/lonicera/factory/TokenFactory.java
+++
@@ -1,7 +0,0 @@
-package com.lonicera.factory;
-
-import com.lonicera.token.Token;
-
-public interface TokenFactory {
-  <T extends Token> T token(Class<T> clazz);
-}
